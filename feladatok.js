e2e87dde47506f35dea9f19148eb269133f74906
marom83 <33879457+marom83@users.noreply.github.com>
2021-12-29 10:57:41 +0100

    adoazonosito

--- feladatok.js
+++ feladatok.js
@@ -1,11 +1,3 @@
-// 1. feladat
-
-/*A TAJ szám egy kilenc számjegyből álló szám, amelyben az első nyolc számjegy egy folyamatosan kiadott egyszerű sorszám,
-amely mindig az előző, utoljára kiadott sorszámból egy hozzáadásával keletkezik.
-A kilencedik számjegy ellenőrző ún. CDV kód, melynek képzési algoritmusa az alábbi:
-A TAJ szám első nyolc számjegyéből a páratlan helyen állókat hárommal, a páros helyen állókat héttel szorozzuk,
-és a szorzatokat összeadjuk. Az összeget tízzel elosztva a maradékot tekintjük a kilencedik, azaz CDV kódnak.*/
-
 const isValidTAJ = (taj) => {
     let sum3 = 0, sum7 = 0;
     for(let i = 0; i < taj.length-1; i++) {
@@ -20,12 +12,22 @@ const isValidTAJ = (taj) => {
     return Number(taj[8]) === residue;
 }
 
-/*születés óta eltelt napok száma + ellenőrző összeg*/
-/*
-const isvalidAdoazon = (adoazon) => {
+
+const isValidAdoazon = (adoazon) => {
     let conditional1 = Number(adoazon[0]) === 8;
 
-    let conditional2 =
+    let char26 = Number(adoazon.slice(1, 6));
+    
+    let char79 = Number(adoazon.slice(7, 9));
+
+    let szumma = 0;
+    for(let i=0; i<adoazon.length - 1; i++) {
+        szumma += Number(adoazon[i])*(i+1);
+    }
+    let conditional2 = szumma % 11 === Number(adoazon[9]);
+    
+    let conditional3 = szumma % 11 !== 10;
+
     return conditional1 && conditional2 && conditional3;
 }
-*/
+
